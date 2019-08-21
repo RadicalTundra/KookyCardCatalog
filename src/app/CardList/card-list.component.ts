6e66776b33a9cdb46bacdb29fbfd96c1fafac8d3
bitstudent <tgrady4@my.wctc.edu>
2019-08-21 16:40:44 -0500

    get the main page working with angular CLI server

---
+++ src/app/CardList/card-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './card-list.component.html',
+  styleUrls: ['../app.component.css']
+})
+export class CardListComponent {
+  title = 'Yeet dab';
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// @ts-ignore
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Home/home.component';
+import { YeetListComponent } from './YeetList/yeet-list.component';
+import { CardListComponent } from './CardList/card-list.component';
+
+const appRoutes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: '', component: HomeComponent},
+  {path: 'yeet-center', component: YeetListComponent},
+  {path: 'cards', component: CardListComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    YeetListComponent,
+    CardListComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- for debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
